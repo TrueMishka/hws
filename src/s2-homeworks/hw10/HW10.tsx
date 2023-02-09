34ccd8ddaa4c2e22d523b30ba2b0deb028de213c
TrueMishka <Mixxxa32123@gmail.com>
2023-02-09 16:06:42 +0300

    hw10

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import {AppStoreType} from './bll/store'
-import {loadingAC} from './bll/loadingReducer'
+import {loadingAC, LoadingStateType} from './bll/loadingReducer'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import s2 from '../../s1-main/App.module.css'
 import {Loader} from './Loader'
@@ -15,12 +15,14 @@ import {Loader} from './Loader'
 
 const HW10 = () => {
     // useSelector, useDispatch // пишет студент
-    const isLoading = false
+    const dispatch = useDispatch()
+    const isLoading = useSelector<AppStoreType, LoadingStateType>(state => state.loading)
 
     const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
         // dispatch
-
+        dispatch(loadingAC(true))
         // setTimeout
+        setTimeout(() => dispatch(loadingAC(false)), 1500)
     }
 
     return (
@@ -28,7 +30,7 @@ const HW10 = () => {
             <div className={s2.hwTitle}>Homework #10</div>
 
             <div className={s2.hw}>
-                {isLoading ? (
+                {isLoading.isLoading ? (
                     <div id={'hw10-loading'}>
                         <Loader/>
                     </div>
--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,12 @@
 import s from './Loader.module.css'
+import {Box, CircularProgress} from "@mui/material";
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => {
+    return (
+        <div className={s.loader}>
+            <Box sx={{ display: 'flex' }}>
+                <CircularProgress />
+            </Box>
+        </div>
+    )
+}
